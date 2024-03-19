562ba28b7a786d75f49f01e35188ccc51952b8aa
Aldrian <achinatuzi@gmail.com>
2024-03-19 20:13:57 +0100

    vb

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -16,7 +16,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyparser.json({ limit: "50mb" }));
 app.use(bodyparser.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors());
+app.use( cors());
 
 const MONGODB_URI =
   process.env.MONGODB_URI ||
