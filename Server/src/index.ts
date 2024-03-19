aae6d7580db3ee0bf72dcc45d81539ae725c3957
Aldrian <achinatuzi@gmail.com>
2024-03-19 15:58:16 +0100

    jghjg

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -28,12 +28,13 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyparser.json({ limit: "50mb" }));
 app.use(bodyparser.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
 
 app.use("/api/seed", seedRouter);
 app.use("/api/users", userRouter);
 app.use("/api/events", eventRouter);
 
-app.use(cors());
+
 app.use(function (req, res, next) {
     //Enabling CORS
     res.setHeader(
