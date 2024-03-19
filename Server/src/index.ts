7b25da19af4282dba8b64909f84f4a2e792e9cd7
Aldrian <achinatuzi@gmail.com>
2024-03-19 19:01:40 +0100

    df

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -10,6 +10,12 @@ const userRouter = require("./routers/userRouter");
 const app = express();
 dotenv.config();
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyparser.json({ limit: "50mb" }));
+app.use(bodyparser.urlencoded({ limit: "50mb", extended: true }));
+app.use(cors());
+
 const MONGODB_URI =
   process.env.MONGODB_URI ||
   "mongodb+srv://gvcServer:cI5BP4ROOFGms4DP@gvc.ln1ctz9.mongodb.net/";
@@ -23,11 +29,7 @@ mongoose
     console.log("error mongodb");
   });
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(bodyparser.json({ limit: "50mb" }));
-app.use(bodyparser.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors());
+
 
 app.use("/api/seed", seedRouter);
 app.use("/api/users", userRouter);
