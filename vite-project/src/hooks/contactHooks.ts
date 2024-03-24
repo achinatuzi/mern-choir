587bfccf74d668a96febfc68703a3cfe69a249a7
Aldrian <achinatuzi@gmail.com>
2024-03-24 18:41:36 +0100

    commit

--- vite-project/src/hooks/contactHooks.ts
+++ vite-project/src/hooks/contactHooks.ts
@@ -1,11 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import apiClient from "../apiClient";
 import { Contact } from "../types/Contact";
-import axios from "axios";
+
 
 export const useGetContactHistoryQuery = () =>
   useQuery({
     queryKey: ["contacts"],
     queryFn: async () =>
-      (await axios.get<Contact[]>(`http://localhost:4000/api/email`)).data,
+      (await apiClient.get<Contact[]>(`api/email`)).data,
   });
