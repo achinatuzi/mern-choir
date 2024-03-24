39ab2eff70e5cfa0d7b3bd2fddf724b1536214f0
Aldrian <achinatuzi@gmail.com>
2024-03-24 23:42:33 +0100

    fdf

--- Server/src/models/userModel.ts
+++ Server/src/models/userModel.ts
@@ -85,7 +85,7 @@ export class User {
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
       "Please fill a valid email address",
     ],
-    unique: true,
+    // unique: true,
     trim: true,
   })
   public email!: string;
